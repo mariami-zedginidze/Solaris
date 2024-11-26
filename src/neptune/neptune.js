7ef3bd841b9aa0ac2f580b2ec24b220b44d9132e
mariami-zedginidze <zedginidze.marie@gmail.com>
2024-11-26 11:30:27 +0400

    Updated neptune.js file and script.js files

--- src/neptune/neptune.js
+++ src/neptune/neptune.js
@@ -1,37 +1,55 @@
 import * as THREE from 'three';
-import neptuneVertexShader from './vertex.glsl';
-import neptuneFragmentShader from './fragment.glsl';
-const neptuneDayTexture = textureLoader.load('/textures/neptune/neptune.jpg');
 
 export default class Neptune {
-    constructor(scene, textureLoader) {
-        this.scene = scene;
+    constructor(textureLoader) {
+        // Neptune Texture
+        const neptuneDayTexture = textureLoader.load('/textures/neptune/neptune.jpg');
+        neptuneDayTexture.colorSpace = THREE.SRGBColorSpace;
+        neptuneDayTexture.anisotropy = 8;
 
-        // Load Neptune texture
-        this.neptuneDayTexture = textureLoader.load('./neptune/neptune.jpg');
-        this.neptuneDayTexture.colorSpace = THREE.SRGBColorSpace;
-        this.neptuneDayTexture.anisotropy = 8;
+        // Colors
+        const nightColor = new THREE.Vector4(0.0, 0.0, 0.0, 0.9);
 
-        // Create Neptune material
-        this.neptuneMaterial = new THREE.ShaderMaterial({
+        // Neptune Mesh
+        const neptuneGeometry = new THREE.SphereGeometry(2, 64, 64);
+        this.material = new THREE.ShaderMaterial({
             vertexShader: neptuneVertexShader,
             fragmentShader: neptuneFragmentShader,
             uniforms: {
-                uDayTexture: { value: this.neptuneDayTexture },
-                uNightColor: { value: new THREE.Vector4(0.0, 0.0, 0.0, 0.9) },
-                uSunDirection: { value: new THREE.Vector3(0, 0, 1) },
+                uDayTexture: new THREE.Uniform(neptuneDayTexture),
+                uNightColor: { value: nightColor },
+                uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
                 uAtmosphereDayColor: { value: new THREE.Color('#3a0ca3') },
-                uAtmosphereTwilightColor: { value: new THREE.Color('#7209b7') },
-            },
+                uAtmosphereTwilightColor: { value: new THREE.Color('#7209b7') }
+            }
         });
 
-        // Create Neptune geometry and mesh
-        const neptuneGeometry = new THREE.SphereGeometry(2, 64, 64);
-        this.neptuneMesh = new THREE.Mesh(neptuneGeometry, this.neptuneMaterial);
-        this.scene.add(this.neptuneMesh);
+        this.mesh = new THREE.Mesh(neptuneGeometry, this.material);
+
+        // Atmosphere
+        this.atmosphereMaterial = new THREE.ShaderMaterial({
+            side: THREE.BackSide,
+            transparent: true,
+            vertexShader: atmosphereVertexShader,
+            fragmentShader: atmosphereFragmentShader,
+            uniforms: {
+                uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
+                uAtmosphereDayColor: { value: new THREE.Color('#3a0ca3') },
+                uAtmosphereTwilightColor: { value: new THREE.Color('#7209b7') }
+            }
+        });
+
+        const atmosphere = new THREE.Mesh(neptuneGeometry, this.atmosphereMaterial);
+        atmosphere.scale.set(1.04, 1.04, 1.04);
+
+        // Group Neptune and its Atmosphere
+        this.group = new THREE.Group();
+        this.group.add(this.mesh);
+        this.group.add(atmosphere);
     }
 
     updateSunDirection(sunDirection) {
-        this.neptuneMaterial.uniforms.uSunDirection.value.copy(sunDirection);
+        this.material.uniforms.uSunDirection.value.copy(sunDirection);
+        this.atmosphereMaterial.uniforms.uSunDirection.value.copy(sunDirection);
     }
 }
